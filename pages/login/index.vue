b40451f2cb310918600d98a96f87b677fccb347e
lipengzhou <lpzmail@163.com>
2020-06-17 18:50:53 +0800

    登录注册-实现基本登录功能

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -15,15 +15,15 @@
             <li>That email is already taken</li>
           </ul>
 
-          <form>
+          <form @submit.prevent="onSubmit">
             <fieldset v-if="!isLogin" class="form-group">
               <input class="form-control form-control-lg" type="text" placeholder="Your Name">
             </fieldset>
             <fieldset class="form-group">
-              <input class="form-control form-control-lg" type="text" placeholder="Email">
+              <input v-model="user.email" class="form-control form-control-lg" type="text" placeholder="Email">
             </fieldset>
             <fieldset class="form-group">
-              <input class="form-control form-control-lg" type="password" placeholder="Password">
+              <input v-model="user.password" class="form-control form-control-lg" type="password" placeholder="Password">
             </fieldset>
             <button class="btn btn-lg btn-primary pull-xs-right">
               {{ isLogin ? 'Sign in' : 'Sign up' }}
@@ -37,11 +37,40 @@
 </template>
 
 <script>
+import request from '@/utils/request'
+
 export default {
   name: 'LoginIndex',
   computed: {
     isLogin () {
       return this.$route.name === 'login'
+    }
+  },
+  data () {
+    return {
+      user: {
+        email: '',
+        password: ''
+      }
+    }
+  },
+
+  methods: {
+    async onSubmit () {
+      // 提交表单请求登录
+      const { data } = await request({
+        method: 'POST',
+        url: '/api/users/login',
+        data: {
+          user: this.user
+        }
+      })
+
+      console.log(data)
+      // TODO: 保存用户的登录状态
+
+      // 跳转到首页
+      this.$router.push('/')
     }
   }
 }
