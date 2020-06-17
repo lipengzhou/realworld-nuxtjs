8b44d40f86a5bc2db98f07e1a22a5a57920c8969
lipengzhou <lpzmail@163.com>
2020-06-17 18:17:56 +0800

    项目初始化-导入剩余页面

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,6 +28,26 @@ module.exports = {
               path: '/register',
               name: 'register',
               component: resolve(__dirname, 'pages/login/')
+            },
+            {
+              path: '/profile/:username',
+              name: 'profile',
+              component: resolve(__dirname, 'pages/profile/')
+            },
+            {
+              path: '/settings',
+              name: 'settings',
+              component: resolve(__dirname, 'pages/settings/')
+            },
+            {
+              path: '/editor',
+              name: 'editor',
+              component: resolve(__dirname, 'pages/editor/')
+            },
+            {
+              path: '/article/:slug',
+              name: 'article',
+              component: resolve(__dirname, 'pages/article/')
             }
           ]
         }
