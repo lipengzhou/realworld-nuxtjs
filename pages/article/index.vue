83e72bd9089cae5a5eead687ed3187f36c1c2a83
lipengzhou <lpzmail@163.com>
2020-06-21 23:15:01 +0800

    文章详情-把Markdown转为HTML

--- pages/article/index.vue
+++ pages/article/index.vue
@@ -31,7 +31,7 @@
     <div class="container page">
 
       <div class="row article-content">
-        <div class="col-md-12">{{ article.body }}</div>
+        <div class="col-md-12" v-html="article.body"></div>
       </div>
 
       <hr />
@@ -116,13 +116,17 @@
 
 <script>
 import { getArticle } from '@/api/article'
+import MarkdownIt from 'markdown-it'
 
 export default {
   name: 'ArticleIndex',
   async asyncData ({ params }) {
     const { data } = await getArticle(params.slug)
+    const { article } = data
+    const md = new MarkdownIt()
+    article.body = md.render(article.body)
     return {
-      article: data.article
+      article
     }
   }
 }
