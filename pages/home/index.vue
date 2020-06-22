252d4656b710ed1bd29fa852465d989c93bab175
lipengzhou <lpzmail@163.com>
2020-06-22 21:00:00 +0800

    部署更新测试

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -3,7 +3,7 @@
 
     <div class="banner">
       <div class="container">
-        <h1 class="logo-font">conduit</h1>
+        <h1 class="logo-font">拉勾教育</h1>
         <p>A place to share your knowledge.</p>
       </div>
     </div>
